c8c20f8da65ae73f4fcff382774233297024d330
marconiguimaraes <marconiguimaraes@multitecnica.com.br>
2026-02-18 17:07:04 -0300

    chore: fix gradle sync and remove buildSrc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,24 @@
-// The settings file is the entry point of every Gradle build.
-// Its primary purpose is to define the subprojects.
-// It is also used for some aspects of project-wide configuration, like managing plugins, dependencies, etc.
-// https://docs.gradle.org/current/userguide/settings_file_basics.html
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
 dependencyResolutionManagement {
-    // Use Maven Central as the default repository (where Gradle will download dependencies) in all subprojects.
-    @Suppress("UnstableApiUsage")
     repositories {
+        google()
         mavenCentral()
     }
 }
 
-plugins {
-    // Use the Foojay Toolchains plugin to automatically download JDKs required by subprojects.
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
 rootProject.name = "http-event-pool"
-include("core")
-include("annotations")
-include("retrofit")
-include("compose")
-include("sample")
+
+include(
+    "core",
+    "annotations",
+    "retrofit",
+    "compose",
+    "sample"
+)
